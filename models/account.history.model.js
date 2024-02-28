cd666f08ceffbd2d97c554daf62565f27a7305ba
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-02-28 11:59:12 -0500

    created data model for user account history and added array of transactions

---
+++ models/account.history.model.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: 1,
+    item_name: "Freelance Work",
+    amount: 600,
+    date: "2024-02-26",
+    from: "Client",
+    category: "Income",
+  },
+  {
+    id: 2,
+    item_name: "Electric Bill",
+    amount: -80,
+    date: "2024-02-25",
+    from: "Utility Company",
+    category: "Utilities",
+  },
+  {
+    id: 3,
+    item_name: "Dinner Out",
+    amount: -50,
+    date: "2024-02-24",
+    from: "Restaurant",
+    category: "Dining",
+  },
+  {
+    id: 4,
+    item_name: "Emergency Fund Deposit",
+    amount: -200,
+    date: "2024-02-23",
+    from: "Bank",
+    category: "Savings",
+  },
+];
